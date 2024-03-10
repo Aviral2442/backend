fb488fd1ea6ebd27a8b771fe98f3bde8558e0eb9
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-03-10 17:58:46 +0530

    database

---
+++ connection.js
@@ -0,0 +1,18 @@
+//req command
+const mongoose = require('mongoose');
+
+const url="mongodb+srv://aviral2442:aviral2442@cluster0.lilbdzb.mongodb.net/mynewdatabase?retryWrites=true&w=majority&appName=Cluster0";
+
+// asynchronous function - return promise
+mongoose.connect(url)
+
+// thenc command - this command help you to see whether url is working aur throuhing error
+.then((result) => {
+    console.log('database connected');
+    
+}).catch((err) => {
+    console.log('err');
+    
+});
+
+console.log('last line');
