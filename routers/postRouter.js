950b5b45d3f3bac80d5c73718d6d2d91a571d754
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-03-13 18:34:45 +0530

    database connected

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Model = require('../models/postModel');
 
 router.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('post add operation');
+
+    new Model(req.body).save() // to save data in mongo db
+
+    // thenc 
+
+    .then((result) => {
+        res.status(200).json(result);  // status code 200 meNS DATA is saved in mongo db just like error 404
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);     // status code 500 
+
+        // response status code
+        // 100 - 199 - informational
+        // 200 -299 - success
+        // 300 - 399 - 
+        // 400 499 - client side error
+        // 500 - 599 - server / backend side error
+        
+    });
+
+  //   res.send('post add operation');  only for response
 });
 
 router.get('/getall', (req, res) => {
